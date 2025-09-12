b0e7b685efd7f710cbdff6920b31c4ef79654ac3
Bahodirxon <bohodirxon.mahmudoov@gmail.com>
2025-09-12 22:56:34 +0500

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 function Home() {
   return (
     <>
-      <h1>Home</h1>
-      {/* <h1 className="homeTitle">
-        <span>Healthy</span> meals, zero fuss
-      </h1> */}
+      <div className="homeTitleBack"></div>
+      <h1 className="homeTitle">
+        <span className="colText">Healthy</span> meals, zero fuss
+      </h1>
+      <p className="homeDesc">
+        Discover eight quick, whole-food recipes that you can cook tonight—no
+        processed junk, no guesswork.
+      </p>
+      <button className="btn btn__main">Start exploring</button>
+      <div className="mainPic">
+        <img className="homePat" src="/assets/pattern-squiggle-1.svg" alt="" />
+        <img className="mbpattern" src="/assets/mbatt.svg" alt="" />
+        <img
+          className="main__hero__img"
+          src="/assets/image-home-hero-large.webp"
+          alt=""
+        />
+      </div>
     </>
   );
 }
