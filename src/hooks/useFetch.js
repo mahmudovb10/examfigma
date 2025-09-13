9da69d7c79170ecc623ad2a814b93b0019e1c002
Bahodirxon <bohodirxon.mahmudoov@gmail.com>
2025-09-13 18:03:25 +0500

    updated

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        console.log("API response:", res.data);
+        setData(res.data);
+      } catch (err) {
+        console.error("Fetch error:", err.message);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, [url]);
+
+  return { data, loading, error };
+};
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,7 +1,63 @@
+import { useFetch } from "../hooks/useFetch";
+import { useState } from "react";
+
 function Recipes() {
+  const { data, loading, error } = useFetch(
+    "https://json-api.uz/api/project/recipes/recipes"
+  );
+
+  if (loading) return <p>Yuklanmoqda...</p>;
+  if (error) return <p>Xatolik: {error}</p>;
+
+  const recipes = data?.data || data || [];
+
   return (
     <div>
-      <h1>recipes</h1>
+      <h1 className="recTitle">Explore our simple, healthy recipes</h1>
+      <p className="recDesc">
+        Discover eight quick, whole-food dishes that fit real-life schedules and
+        taste amazing. Use the search bar to find a recipe by name or
+        ingredient, or simply scroll the list and let something delicious catch
+        your eye.
+      </p>
+
+      <div className="card-grid">
+        {recipes.map((recipe) => (
+          <div className="card" key={recipe.id}>
+            <picture>
+              <source media="(max-width: 768px)" srcSet={recipe.image.small} />
+              <img
+                src={recipes.image}
+                alt={recipe.title}
+                className="card-img"
+              />
+            </picture>
+            <div className="card-body">
+              <h3 className="card-title">{recipe.title}</h3>
+              <p className="card-desc">{recipe.overview}</p>
+
+              <div className="card-info">
+                <div className="act">
+                  <div className="serving">
+                    <img src="/assets/icon-servings.svg" alt="" /> Servings:{" "}
+                    {recipe.servings}
+                  </div>
+                  <div className="prep">
+                    <img src="/assets/icon-prep-time.svg" alt="" /> Prep:{" "}
+                    {recipe.prepMinutes} mins
+                  </div>
+                </div>
+                <div className="cook">
+                  <img src="/assets/icon-cook-time.svg" alt="" /> Cook:{" "}
+                  {recipe.cookMinutes} mins
+                </div>
+              </div>
+
+              <button className="card-btn">View Recipe</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
